4d2f9369e00ef63ce56947c33ce4c7307f67dbe9
Elise St Hilaire and Nicole Sanders <elise+nicole@>
2017-03-28 15:04:06 -0700

    add functionality to edit each ingredient individually

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,19 +9,21 @@ import { Component } from '@angular/core';
     <div *ngFor="let recipe of recipes">
       <h3 (click)="recipe.currentRecipe = !recipe.currentRecipe">{{recipe.title}}</h3>
       <div *ngIf="recipe.currentRecipe">
-        <li *ngFor="let ingredient of recipe.ingredients">{{ingredient}}</li>
+        <li *ngFor="let ingredient of recipe.ingredients">{{ingredient.name}}</li>
         <p>{{recipe.directions}}</p>
-        <button (click)="recipe.chooseToEditRecipe = !recipe.chooseToEditRecipe" (click)="editRecipe(recipe)" class="btn btn-info">Edit</button>
+        <button (click)="recipe.chooseToEditRecipe = !recipe.chooseToEditRecipe" class="btn btn-info">Edit</button>
       </div>
       <div *ngIf="recipe.chooseToEditRecipe">
         <h3>Edit Recipe</h3>
         <label>Enter Recipe Title</label>
-        <input [(ngModel)]="chooseToEditRecipe.title">
+        <input [(ngModel)]="recipe.title">
         <label>Enter Ingredients</label>
-        <input [(ngModel)]="chooseToEditRecipe.ingredients">
+        <div *ngFor="let ingredient of recipe.ingredients">
+          <input [(ngModel)]="ingredient.name">
+        </div>
         <label>Enter Directions</label>
-        <input [(ngModel)]="chooseToEditRecipe.directions">
-        <button (click)="finishedEditing()">Done</button>
+        <input [(ngModel)]="recipe.directions">
+        <button (click)="recipe.chooseToEditRecipe = !recipe.chooseToEditRecipe">Done</button>
       </div>
     </div>
   </div>
@@ -34,16 +36,16 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   recipes: Recipe[] = [
-    new Recipe('Spaghetti', ['2.5 C Sauce', '1 stewed tomato', '1 Box Noodles'],'Cook noodles and throw on some sauce and the stewed tomato.'),
-    new Recipe('Baked Potatoes', ['5 potatoes', '2 TB Veggie Oil', '1 tsp Salt'], 'Preheat oven to 375. While preheating, smother potatoes in Veggie oil and sprinkle salt. Put them on the baking sheet and cook for an hour.'),
-    new Recipe('Banana and PB', ['1 Banana', '2 TB Peanut Butter'], 'Put some peanut butter on the banana and enjoy.')
+    new Recipe('Spaghetti', [new Ingredient('2.5 C Sauce'), new Ingredient('1 stewed tomato'), new Ingredient('1 Box Noodles')],'Cook noodles and throw on some sauce and the stewed tomato.'),
+    new Recipe('Baked Potatoes', [new Ingredient('5 potatoes'), new Ingredient('2 TB Veggie Oil'), new Ingredient('1 tsp Salt')], 'Preheat oven to 375. While preheating, smother potatoes in Veggie oil and sprinkle salt. Put them on the baking sheet and cook for an hour.'),
+    new Recipe('Banana and PB', [new Ingredient('1 Banana'), new Ingredient('2 TB Peanut Butter')], 'Put some peanut butter on the banana and enjoy.')
   ];
 
-  chooseToEditRecipe = null;
+  // chooseToEditRecipe = null;
 
-  editRecipe(clickedRecipe) {
-    this.chooseToEditRecipe = clickedRecipe;
-  }
+  // editRecipe(clickedRecipe) {
+  //   this.chooseToEditRecipe = clickedRecipe;
+  // }
 
   finishedEditing() {
     this.chooseToEditRecipe = null;
@@ -51,5 +53,9 @@ export class AppComponent {
 }
 
 export class Recipe {
-  constructor (public title: string, public ingredients: string[], public directions: string) {}
+  constructor (public title: string, public ingredients: Ingredient[], public directions: string) {}
+}
+
+export class Ingredient {
+  constructor (public name: string) {}
 }
