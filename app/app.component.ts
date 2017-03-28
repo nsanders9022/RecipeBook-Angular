4d1305e17eb5a488cc451dc98e2a1837a2f5463e
Elise St Hilaire and Nicole Sanders <elise+nicole@>
2017-03-28 11:49:51 -0700

    instantiate currentRecipe for each listed recipe in order to hide and show each recipe

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,9 +7,11 @@ import { Component } from '@angular/core';
     <h1>Recipe Box</h1>
     <h3>Date: {{month}}/{{day}}/{{year}}</h3>
     <div *ngFor="let recipe of recipes">
-      <h3>{{recipe.title}}</h3>
-      <li *ngFor="let ingredient of recipe.ingredients">{{ingredient}}</li>
-      <p>{{recipe.directions}}</p>
+      <h3 (click)="recipe.currentRecipe = !recipe.currentRecipe">{{recipe.title}}</h3>
+      <div *ngIf="recipe.currentRecipe">
+        <li *ngFor="let ingredient of recipe.ingredients">{{ingredient}}</li>
+        <p>{{recipe.directions}}</p>
+      </div>
     </div>
   </div>
   `
@@ -25,6 +27,10 @@ export class AppComponent {
     new Recipe('Baked Potatoes', ['5 potatoes', '2 TB Veggie Oil', '1 tsp Salt'], 'Preheat oven to 375. While preheating, smother potatoes in Veggie oil and sprinkle salt. Put them on the baking sheet and cook for an hour.'),
     new Recipe('Banana and PB', ['1 Banana', '2 TB Peanut Butter'], 'Put some peanut butter on the banana and enjoy.')
   ]
+
+  displayRecipe(clickedRecipe) {
+    this.currentRecipe = clickedRecipe;
+  }
 }
 
 export class Recipe {
